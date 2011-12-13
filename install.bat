158713bbae6c892cc23449c0082146d1814ae6e1
Ethan <wsvn53@gmail.com>
2011-12-13 20:50:55 +0800

    fix install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,15 @@
-@echo "winxp"
-@copy Fedit\\bin\\Release\\Fedit.dll "%userprofile%\\My Documents\\Fiddler2\\Scripts\\"
-@echo "win7/vista"
-@copy Fedit\\bin\\Release\\Fedit.dll "%userprofile%\\Documents\\Fiddler2\\Scripts\\"
+@if exist "C:\\Program Files\\Fiddler2\\" (
+	@echo "Copy Fedit.dll to C:\\Program Files\\Fiddler2\\"
+	copy Fedit\\bin\\Release\\Fedit.dll "C:\\Program Files\\Fiddler2\\Scripts\\"
+)else if exist "D:\\Program Files\\Fiddler2\\" (
+	@echo "Copy Fedit.dll to D:\\Program Files\\Fiddler2\\"
+	copy Fedit\\bin\\Release\\Fedit.dll "D:\\Program Files\\Fiddler2\\Scripts\\"
+)else if exist "%userprofile%\\Documents\\Fiddler2\\" (
+	@echo "Copy Fedit.dll to %userprofile%\\Documents\\Fiddler2\\"
+	copy Fedit\\bin\\Release\\Fedit.dll "%userprofile%\\Documents\\Fiddler2\\Scripts\\"
+)else if exist "%userprofile%\\My Documents\\Fiddler2\\" (
+	@echo "Copy Fedit.dll to %userprofile%\\My Documents\\Fiddler2\\"
+	copy Fedit\\bin\\Release\\Fedit.dll "%userprofile%\\My Documents\\Fiddler2\\Scripts\\"
+)
 @echo "installed."
 @pause
